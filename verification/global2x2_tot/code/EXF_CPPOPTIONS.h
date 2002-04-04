5c36fa345a9f962a7e93161471e68a728d4fba09
Patrick Heimbach <heimbach@mit.edu>
2002-04-04 12:05:47 +0000

    Adding global 2x2 deg. configuration to repository.

---
+++ verification/global2x2_tot/code/EXF_CPPOPTIONS.h
@@ -0,0 +1,7 @@
+C $Header: /u/gcmpack/MITgcm/verification/global2x2_tot/code/Attic/EXF_CPPOPTIONS.h,v 1.1.2.1 2002/04/04 12:05:47 heimbach Exp $
+C
+#include "CPP_OPTIONS.h"
+C 
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C

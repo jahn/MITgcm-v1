9cd69e22f94d19b01582d6e2dcef2500f6f695f8
Patrick Heimbach <heimbach@mit.edu>
2003-05-23 16:34:35 +0000

    New verif. for Tini-Tfin optimization.

---
+++ verification/spinup_optim/code/EXF_CPPOPTIONS.h
@@ -0,0 +1,7 @@
+C $Header: /u/gcmpack/MITgcm/verification/spinup_optim/code/Attic/EXF_CPPOPTIONS.h,v 1.1.2.1 2003/05/23 16:29:47 heimbach Exp $
+C
+#include "CPP_OPTIONS.h"
+C 
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C

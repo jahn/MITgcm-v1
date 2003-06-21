6276a58fc9ab2d6cbda5f216b96d9a7eae59be14
Patrick Heimbach <heimbach@mit.edu>
2003-06-21 23:50:14 +0000

    Starting obcs control verification.

---
+++ verification/natl_box_obcs/code/EXF_CPPOPTIONS.h
@@ -0,0 +1,7 @@
+C $Header: /u/gcmpack/MITgcm/verification/natl_box_obcs/code/Attic/EXF_CPPOPTIONS.h,v 1.1.2.1 2003/06/21 23:50:14 heimbach Exp $
+C
+#include "CPP_OPTIONS.h"
+C 
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C

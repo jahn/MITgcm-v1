1568ce2313684c398381d057fe1b15ce1ee8bdad
Patrick Heimbach <heimbach@mit.edu>
2003-06-24 23:11:37 +0000

    Merging from c51

---
+++ pkg/monitor/MONITOR_OPTIONS.h
@@ -0,0 +1,11 @@
+C $Header: /u/gcmpack/MITgcm/pkg/monitor/MONITOR_OPTIONS.h,v 1.1.2.1 2003/06/24 23:10:27 heimbach Exp $
+C $Name:  $
+
+C CPP options file for monitor package
+C
+C Use this file for selecting options within the monitor package
+
+#include "CPP_OPTIONS.h"
+
+C Disable use of hFacZ
+#undef MONITOR_TEST_HFACZ
